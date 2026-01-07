1312e4f8ba51b37a2383d878ed1a7b2fc8a00c67
Saurabh <saurabh@everything.design>
2026-01-07 17:10:42 +0530

    Initial Commit

---
+++ WebflowAccessManager/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  WebflowAccessManager
+//
+//  Created by Saurabh on 07/01/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ WebflowAccessManager/WebflowAccessManagerApp.swift
@@ -0,0 +1,17 @@
+//
+//  WebflowAccessManagerApp.swift
+//  WebflowAccessManager
+//
+//  Created by Saurabh on 07/01/26.
+//
+
+import SwiftUI
+
+@main
+struct WebflowAccessManagerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
